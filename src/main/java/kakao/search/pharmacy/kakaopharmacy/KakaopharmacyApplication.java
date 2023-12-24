cde702dc9d713da54d2757a5c3b05d664bf08fea
김재령 <wofud0321@naver.com>
2023-12-24 22:35:02 +0900

    first coomit

---
+++ src/main/java/kakao/search/pharmacy/kakaopharmacy/KakaopharmacyApplication.java
@@ -0,0 +1,13 @@
+package kakao.search.pharmacy.kakaopharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaopharmacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kakao/search/pharmacy/kakaopharmacy/KakaopharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package kakao.search.pharmacy.kakaopharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaopharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
