81e722974fdb819d3ef3d851e8998793ff899e19
김재령 <wofud0321@naver.com>
2023-12-27 16:34:52 +0900

    kakaoAPI 호출 서비스

---
+++ src/main/java/kakao/search/pharmacy/kakaopharmacy/service/KakaoApiService.java
@@ -0,0 +1,56 @@
+package kakao.search.pharmacy.kakaopharmacy.service;
+
+import kakao.search.pharmacy.kakaopharmacy.dto.KakaoApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KakaoApiService {
+
+    private final RestTemplate restTemplate;
+
+    private static final String URL = "https://dapi.kakao.com/v2/local/search/address.json";
+    private static final String KEY = "d5f0d71ff26e4d59cec7fa873d0e322c";
+
+    public KakaoApiResponseDto test(String address) throws ParseException {
+
+        // URI 생성
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl(URL)
+                .queryParam("query", address)
+                .build()
+                .toUri();
+        System.out.println("uri = " + uri);
+
+        // 헤더에 인증키 값
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + KEY);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        System.out.println("entity = " + entity);
+
+        /*
+        kakao API 호출: exchange 함수 매개변수
+        1. uri를 String 형태로
+        2. Http 메서드
+        3. HttpEntity
+        4. 반환받을 변수 형식
+         */
+        return restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, KakaoApiResponseDto.class).getBody();
+    }
+}
