e1d8de55d2feecd540ca1fbd717e472a5522d76d
김재령 <wofud0321@naver.com>
2024-01-02 17:28:27 +0900

    약국이름 스키마 추가

--- database/init/direction.sql
+++ database/init/direction.sql
@@ -2,6 +2,7 @@ drop table if exists direction;
 
 create table direction (
                            distance float(53) not null ,
+                           pharmacy_name varchar(255) not null ,
                            latitude float(53) not null ,
                            longitude float(53) not null ,
                            direction_id bigint not null auto_increment,
