770e57a4c7de85053f4f3be58bbd25a8567fdff7
김재령 <wofud0321@naver.com>
2024-01-06 18:40:35 +0900

    shortUrl 받기위한 컨트롤러 생성

---
+++ src/main/java/kakao/search/pharmacy/kakaopharmacy/controller/ShortUrlController.java
@@ -0,0 +1,32 @@
+package kakao.search.pharmacy.kakaopharmacy.controller;
+
+import kakao.search.pharmacy.kakaopharmacy.service.UrlService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ShortUrlController {
+
+    private final UrlService urlService;
+
+    @GetMapping("/dir/{shortUrl}")
+    public String searchUrl(@PathVariable("shortUrl") String shortUrl) throws UnsupportedEncodingException {
+        String url = urlService.getOriginUrlByShortUrl(shortUrl);
+        return "redirect:" + url;
+    }
+
+    @GetMapping("/delete/delete/all")
+    public String deleteUrl() {
+        urlService.deleteAll();
+        return "redirect:/";
+    }
+}
