04200a758096aa1ff328ea2409dd66e43eb2fb97
김재령 <wofud0321@naver.com>
2024-01-08 18:37:08 +0900

    url service코드 완성
    에러처리는 아직

--- src/main/java/kakao/search/pharmacy/kakaopharmacy/service/UrlService.java
+++ src/main/java/kakao/search/pharmacy/kakaopharmacy/service/UrlService.java
@@ -4,11 +4,15 @@ import kakao.search.pharmacy.kakaopharmacy.entity.Url;
 import kakao.search.pharmacy.kakaopharmacy.repository.UrlRepository;
 import kakao.search.pharmacy.kakaopharmacy.util.Base62;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Objects;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UrlService {
@@ -18,28 +22,29 @@ public class UrlService {
     private final UrlRepository urlRepository;
 
     @Transactional
-    public String getOriginUrlByShortUrl(String shortUrl) { // 에러처리 해주기
+    public String getOriginUrlByShortUrl(String shortUrl) { // TODO: 에러처리 해주기
         Optional<Url> originUrl = urlRepository.findByShortUrl(shortUrl);
-        return originUrl.map(Url::getOriginUrl).orElse(null);
+        String url = originUrl.map(Url::getOriginUrl).orElse(null); // 만약 url이 null 값이면 alert창 띄어주기?? 아니면
+
+        log.info("[UrlService getOriginUrlByShortUrl] url : {}", url);
+
+        return UriComponentsBuilder.fromHttpUrl(Objects.requireNonNull(url)).toUriString();
     }
 
     @Transactional
     public String generateShorteningUrl(String originUrl){
-        Optional<Url> shortUrl = urlRepository.findByShortUrl(originUrl);
-        if (shortUrl.isPresent()) {
-            //생성 요청한 원본 url이 존재 할 경우 기존에 생성된 alias 로 제공
-            return shortUrl.get().getShortUrl();
+        Optional<Url> findOriginUrl = urlRepository.findByOriginUrl(originUrl);
+        if(findOriginUrl.isPresent()){
+            return findOriginUrl.get().getShortUrl();
         }
         return generate(originUrl);
     }
 
-    private String generate(String originUrl){
-        String shorteningUrl;
+    private String generate(String originUrl){String shorteningUrl;
         for (int i = 0; i < GENERATE_COUNT; i++) {
-            shorteningUrl = base62.generateHashingAlias(originUrl + i);
+            shorteningUrl = Base62.generateHashingAlias(originUrl + i);
 
-            if (!urlRepository.findByShortUrl(shorteningUrl).isPresent() && shorteningUrl.length() < 8) {
-                Url url = new Url();
+            if (urlRepository.findByShortUrl(shorteningUrl).isEmpty() && shorteningUrl.length() < 8) {
                 urlRepository.save(
                         Url.builder()
                         .shortUrl(shorteningUrl)
@@ -48,7 +53,7 @@ public class UrlService {
                 return shorteningUrl;
             }
         }
-        return "URL을 생성하지 못했습니다.";
+        return "URL을 생성하지 못했습니다.";  // TODO: 에러처리?
     }
 
     @Transactional
